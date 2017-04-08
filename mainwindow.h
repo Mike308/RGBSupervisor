2f14aba42ca171d37ad233a0d7e3c206d05cbdf2
Majkel44 <m.werner9205@gmail.com>
2017-04-08 17:01:13 +0200

    - included animationssetup class - added declaration of slots for pushButton click and getSetup from setup dialog - added declaration of pointer on AnimationsSetup object

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
+
+#include "animationssetup.h"
+
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +19,14 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+    void getSetup(int &type, int &speed, int &step);
+
 private:
     Ui::MainWindow *ui;
+    AnimationsSetup *animationsSetupDialog;
+
 };
 
 #endif // MAINWINDOW_H
