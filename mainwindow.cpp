38afff91207208ec7f509ee018384cbb2a7483fe
Majkel44 <m.werner9205@gmail.com>
2017-04-22 23:48:37 +0200

    - added library for detecting connected serial device - added library for handling serial port - added opening serial port in detected slot

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QPalette>
+#include <QKeyEvent>
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -10,6 +11,12 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     animationsSetupDialog = new AnimationsSetup();
     connect(animationsSetupDialog,SIGNAL(parametersSetup(int&,int&,int&)),this,SLOT(getSetup(int&,int&,int&)));
+    serialDetector = new SerialDetector();
+    connect(serialDetector,SIGNAL(onDetect(int,QSet<QString>)),this,SLOT(onDetected(int,QSet<QString>)));
+    serialPort = new QSerialPort(this);
+
+
+
 
 }
 
@@ -48,7 +55,7 @@ void MainWindow::on_gSlider_valueChanged(int value)
     color.setRgb(ui->rSlider->value(),ui->gSlider->value(),ui->bSlider->value());
     pal.setColor(QPalette::Background,color);
     ui->colorProbe->setPalette(pal);
-     ui->colorProbe->setAutoFillBackground(true);
+    ui->colorProbe->setAutoFillBackground(true);
     ui->gLabel->setText(QString::number(value));
 }
 
@@ -61,5 +68,59 @@ void MainWindow::on_bSlider_valueChanged(int value)
     ui->colorProbe->setPalette(pal);
     ui->colorProbe->setAutoFillBackground(true);
     ui->bLabel->setText(QString::number(value));
+
+}
+
+
+void MainWindow::onDetected(int status, QSet<QString> coms){
+
+    foreach (QString comName, coms) {
+
+        qDebug () << status << " " << comName;
+
+    }
+
+    if (status == 1){
+
+        serialPort->setBaudRate(9600);
+        serialPort->setFlowControl(QSerialPort::NoFlowControl);
+        serialPort->setDataBits(QSerialPort::Data8);
+        serialPort->setStopBits(QSerialPort::OneStop);
+        serialPort->open(QIODevice::ReadWrite);
+
+    }else{
+
+        serialPort->close();
+    }
+
+
+
+
+
+
+
+}
+
+void MainWindow::readyRead(){
+
+
+}
+
+
+
+
+
+
+
+void MainWindow::on_serialCombo_activated(const QString &arg1)
+{
+
+
+
+
+}
+
+void MainWindow::on_serialCombo_highlighted(const QString &arg1)
+{
 
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,9 @@
 
 #include <QMainWindow>
 #include <QDebug>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <3rd/serialdetector.h>
 
 #include "animationssetup.h"
 
@@ -19,6 +22,8 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+
+
 private slots:
     void on_pushButton_clicked();
     void getSetup(int &type, int &speed, int &step);
@@ -29,9 +34,25 @@ private slots:
 
     void on_bSlider_valueChanged(int value);
 
+    void on_serialCombo_activated(const QString &arg1);
+
+    void on_serialCombo_highlighted(const QString &arg1);
+
+    void onDetected(int,QSet<QString>);
+
+    void readyRead();
+
+
+
+
+
 private:
     Ui::MainWindow *ui;
     AnimationsSetup *animationsSetupDialog;
+    SerialDetector * serialDetector;
+    QSerialPort * serialPort;
+
+
 
 };
 
